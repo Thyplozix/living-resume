745590698993db4957455a18641774bc04b9659a
Adrienne Bienapfl <adrienne.bienapfl@fortifiedhq.com>
2023-05-22 12:05:55 -0600

    blah

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -29,7 +29,7 @@ import Layout from '../components/Layout';
  * Making base name for the website (needed in deployment)
  */
 const router = createBrowserRouter(routes, {
-  basename: '/ukko-react-template',
+  basename: '/ukko-react',
 });
 
 function App() {
